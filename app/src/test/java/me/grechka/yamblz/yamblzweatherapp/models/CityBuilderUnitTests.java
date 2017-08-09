5e6018ff963755a5c3d3ba4b2c8881a015b698f0
alexander <st235@yandex.ru>
2017-08-09 13:36:15 +0300

    Implemented models builder coverage

---
+++ app/src/test/java/me/grechka/yamblz/yamblzweatherapp/models/CityBuilderUnitTests.java
@@ -0,0 +1,53 @@
+package me.grechka.yamblz.yamblzweatherapp.models;
+
+import org.junit.Test;
+
+import me.grechka.yamblz.yamblzweatherapp.models.response.city.CityLocation;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by alexander on 09/08/2017.
+ */
+
+public class CityBuilderUnitTests {
+
+    @Test
+    public void CityBuilder_createCityWithBuilder_success() {
+        City city = new City.Builder()
+                .title("Moscow")
+                .placeId("146")
+                .extendedTitle("Moscow, Russia")
+                .location(new CityLocation(0, 0))
+                .isActive(false)
+                .build();
+
+        assertEquals(city.getTitle(), "Moscow");
+        assertEquals(city.getPlaceId(), "146");
+        assertEquals(city.getExtendedTitle(), "Moscow, Russia");
+        assertEquals(city.getLocation(), new CityLocation(0, 0));
+        assertEquals(city.isActive(), false);
+    }
+
+    @Test
+    public void CityBuilder_copyCityAndEditWithBuilder_success() {
+        City moscow = new City.Builder()
+                .title("Moscow")
+                .placeId("146")
+                .extendedTitle("Moscow, Russia")
+                .location(new CityLocation(0, 0))
+                .isActive(false)
+                .build();
+
+        City city = new City.Builder(moscow)
+                .placeId("122")
+                .isActive(true)
+                .build();
+
+        assertEquals(city.getTitle(), "Moscow");
+        assertEquals(city.getPlaceId(), "122");
+        assertEquals(city.getExtendedTitle(), "Moscow, Russia");
+        assertEquals(city.getLocation(), new CityLocation(0, 0));
+        assertEquals(city.isActive(), true);
+    }
+}
---
+++ app/src/test/java/me/grechka/yamblz/yamblzweatherapp/models/WeatherBuilderUnitTests.java
@@ -0,0 +1,79 @@
+package me.grechka.yamblz.yamblzweatherapp.models;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by alexander on 09/08/2017.
+ */
+
+public class WeatherBuilderUnitTests {
+
+    @Test
+    public void WeatherBuilder_createWeatherWithBuilder_success() {
+        Weather weather = new Weather.Builder()
+                .weatherId(800)
+                .humidity(100.0f)
+                .pressure(12.0f)
+                .temperature(100.0f)
+                .minTemperature(100.0f)
+                .maxTemperature(100.0f)
+                .windDegree(5.0f)
+                .windSpeed(1.0f)
+                .clouds(1)
+                .updateTime(1000L)
+                .sunRiseTime(1000L)
+                .sunSetTime(1000L)
+                .build();
+
+        assertEquals(weather.getWeatherId(), 800);
+        assertEquals(weather.getHumidity(), 100.0f);
+        assertEquals(weather.getPressure(), 12.0f);
+        assertEquals(weather.getTemperature(), 100.0f);
+        assertEquals(weather.getMinTemperature(), 100.0f);
+        assertEquals(weather.getMaxTemperature(), 100.0f);
+        assertEquals(weather.getWindDegree(), 5.0f);
+        assertEquals(weather.getWindSpeed(), 1.0f);
+        assertEquals(weather.getClouds(), 1);
+        assertEquals(weather.getUpdateTime(), 1000L);
+        assertEquals(weather.getSunRiseTime(), 1000L);
+        assertEquals(weather.getSunSetTime(), 1000L);
+    }
+
+    @Test
+    public void WeatherBuilder_copyWeatherAndEditWithBuilder_success() {
+        Weather laWeather = new Weather.Builder()
+                .weatherId(800)
+                .humidity(100.0f)
+                .pressure(12.0f)
+                .temperature(100.0f)
+                .minTemperature(100.0f)
+                .maxTemperature(100.0f)
+                .windDegree(5.0f)
+                .windSpeed(1.0f)
+                .clouds(1)
+                .updateTime(1000L)
+                .sunRiseTime(1000L)
+                .sunSetTime(1000L)
+                .build();
+
+        Weather weather = new Weather.Builder(laWeather)
+                .clouds(2)
+                .weatherId(200)
+                .build();
+
+        assertEquals(weather.getWeatherId(), 200);
+        assertEquals(weather.getHumidity(), 100.0f);
+        assertEquals(weather.getPressure(), 12.0f);
+        assertEquals(weather.getTemperature(), 100.0f);
+        assertEquals(weather.getMinTemperature(), 100.0f);
+        assertEquals(weather.getMaxTemperature(), 100.0f);
+        assertEquals(weather.getWindDegree(), 5.0f);
+        assertEquals(weather.getWindSpeed(), 1.0f);
+        assertEquals(weather.getClouds(), 2);
+        assertEquals(weather.getUpdateTime(), 1000L);
+        assertEquals(weather.getSunRiseTime(), 1000L);
+        assertEquals(weather.getSunSetTime(), 1000L);
+    }
+}
