351e86d7b9d0b3b73152a4283948c1be4c6fd019
alexander <st235@yandex.ru>
2017-08-11 16:24:06 +0300

    Implemented city search presenter view

--- app/src/test/java/me/grechka/yamblz/yamblzweatherapp/presentation/citySearch/CitySearchPresenterTest.java
+++ app/src/test/java/me/grechka/yamblz/yamblzweatherapp/presentation/citySearch/CitySearchPresenterTest.java
@@ -5,15 +5,19 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import io.reactivex.Completable;
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import me.grechka.yamblz.yamblzweatherapp.base.BaseTest;
 import me.grechka.yamblz.yamblzweatherapp.data.DatabaseRepository;
 import me.grechka.yamblz.yamblzweatherapp.data.NetworkRepository;
 import me.grechka.yamblz.yamblzweatherapp.models.City;
+import me.grechka.yamblz.yamblzweatherapp.models.response.city.CityLocation;
 import me.grechka.yamblz.yamblzweatherapp.utils.RxSchedulers;
 
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
@@ -49,6 +53,7 @@ public class CitySearchPresenterTest extends BaseTest {
 
         when(networkRepository.obtainSuggestedCities(anyString())).thenReturn(Observable.just(city));
         when(schedulers.getIoToMainTransformer()).thenReturn(observer -> observer);
+        when(networkRepository.obtainCityLocation(anyString())).thenReturn(Single.just(new CityLocation()));
     }
 
     @Test
@@ -63,12 +68,29 @@ public class CitySearchPresenterTest extends BaseTest {
 
     @Test
     public void setObservable() {
-        citySearchPresenter.setObservable(Observable.just("text"));
+        citySearchPresenter.setObservable(Observable.just("test"));
 
         verify(view, atLeastOnce()).clearSuggestions();
         verify(view, atLeastOnce()).showLoading();
         verify(networkRepository).obtainSuggestedCities(anyString());
         verify(view, atLeastOnce()).hideLoading();
         verify(view, atLeastOnce()).addSuggestion(any(City.class));
+    }
+
+    @Test
+    public void fetchSuggestions() {
+        citySearchPresenter.fetchSuggestions("test");
+
+        verify(networkRepository).obtainSuggestedCities(anyString());
+        verify(view, atLeastOnce()).hideLoading();
+        verify(view, atLeastOnce()).addSuggestion(any(City.class));
+    }
+
+    @Test
+    public void fetchCity() {
+        citySearchPresenter.fetchCity(new City.Builder().build());
+
+        verify(networkRepository).obtainCityLocation(anyString());
+        verify(databaseRepository).addCity(any(City.class));
     }
 }
